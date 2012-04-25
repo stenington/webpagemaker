6538f118a5cda5076759fbe479cb244d6a1481d4
Atul Varma <varmaa@gmail.com>
2012-04-25 08:47:26 -0500

    Changed views to use simon wex's bleach fork. Note that this requires developers to substitute vendor/src/bleach with wex's repository, which isn't great, but at least this allows us to write tests for the "real" expected behavior of the API for now. I've added a test in the code that raises an exception if wex's fork isn't detected to help devs. (It doesn't technically check for wex's fork, but it's close enough for now--just makes sure that the developer isn't using the older version of bleach that comes w/ playdoh.)

--- webpagemaker/api/views.py
+++ webpagemaker/api/views.py
@@ -4,7 +4,35 @@ from django.views.decorators.http import require_POST
 from django.shortcuts import get_object_or_404
 from django.http import HttpResponse, HttpResponseBadRequest
 
+import bleach
+
 from . import models
+
+ALLOWED_TAGS = [
+    "!doctype", "html", "body", "a", "abbr", "address", "area", "article",
+    "aside", "audio", "b", "base", "bdi", "bdo", "blockquote", "body", "br",
+    "button", "canvas", "caption", "cite", "code", "col", "colgroup",
+    "command", "datalist", "dd", "del", "details", "dfn", "div", "dl", "dt",
+    "em", "embed", "fieldset", "figcaption", "figure", "footer", "form",
+    "h1", "head", "header", "hgroup", "hr", "html", "i", "iframe", "img",
+    "input", "ins", "keygen", "kbd", "label", "legend", "li", "link", "map",
+    "mark", "menu", "meta", "meter", "nav", "noscript", "object",
+    "ol", "optgroup", "option", "output", "p", "param", "pre", "progress",
+    "q", "rp", "rt", "s", "samp", "section", "select", "small", "source", 
+    "span", "strong", "style", "sub", "summary", "sup", "table", "tbody", 
+    "td", "textarea", "tfoot", "th", "thead", "time", "title", "tr", "track", 
+    "u", "ul", "var", "video", "wbr"
+    ]
+
+ALLOWED_ATTRS = {
+    # TODO: We should probably add to this. What meta attributes can't
+    # be abused for SEO purposes?
+    "meta": ["charset"]
+}
+
+if bleach.VERSION < (1, 1, 1):
+    raise Exception("Please use simon wex's bleach fork for now: " +
+                    "https://github.com/simonwex/bleach.git")
 
 def home(request):
     return HttpResponse("Hello, world!", content_type="text/plain")
@@ -25,6 +53,9 @@ def publish_page(request):
 
 def get_page(request, page_id):
     page = get_object_or_404(models.Page, pk=page_id)
-    response = HttpResponse(page.html)
+    html = bleach.clean(page.html, strip=True, strip_comments=False,
+                        tags=ALLOWED_TAGS, attributes=ALLOWED_ATTRS,
+                        parse_as_fragment=False)
+    response = HttpResponse(html)
     response['Access-Control-Allow-Origin'] = '*'
     return response
