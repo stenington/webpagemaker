cb5c134eeb8dd370b92a3ec13cec3fff88c66a4f
Atul Varma <varmaa@gmail.com>
2012-04-26 12:18:48 -0500

    changed how sanitization tests are written again.

--- webpagemaker/api/tests/test_sanitize.py
+++ webpagemaker/api/tests/test_sanitize.py
@@ -1,14 +1,13 @@
 """
-    This test suite makes tests out of all the HTML files in the
+    This test suite makes tests out of all the subdirectories of the
     'html' directory.
     
-    Suppose there is a file called 'foo.in.html'. This suite will
+    Suppose there is directory called 'foo' in 'html'. This suite will
     generate a test called test_foo_sanitization which loads
-    'foo.in.html', sanitizes it, and compares the result to 'foo.out.html'.
+    'foo/in.html', sanitizes it, and compares the result to 'foo/out.html'.
 """
 
 import os
-import glob
 
 from nose.tools import eq_
 from nose.plugins.skip import SkipTest
@@ -17,26 +16,23 @@ from webpagemaker.api import sanitize
 
 ROOT = os.path.dirname(__file__)
 HTML_DIR = os.path.join(ROOT, 'html')
-TEST_PAGES = glob.glob(os.path.join(HTML_DIR, '*.out.html'))
 
 def init():
-    def make_test(published_path):
-        published_filename = os.path.basename(published_path)
-        testname = published_filename.split('.')[0]
-        input_filename = "%s.in.html" % testname
-        input_path = os.path.join(HTML_DIR, input_filename)
-        
+    def make_test(dirname):
         def test():
+            input_path = os.path.join(HTML_DIR, dirname, 'in.html')
+            output_path = os.path.join(HTML_DIR, dirname, 'out.html')
             input_html = open(input_path, 'r').read()
             if input_html.startswith('<!-- SKIP THIS TEST'):
                 raise SkipTest()
-            published_html = open(published_path, 'r').read()
-            eq_(sanitize.sanitize(input_html), published_html)
+            output_html = open(output_path, 'r').read()
+            eq_(sanitize.sanitize(input_html), output_html)
         
-        test.__name__ = 'test_%s_sanitization' % testname
+        test.__name__ = 'test_%s_sanitization' % dirname
         globals()[test.__name__] = test
         
-    for published_path in TEST_PAGES:
-        make_test(published_path)
+    for dirname in os.listdir(HTML_DIR):
+        if os.path.isdir(os.path.join(HTML_DIR, dirname)):
+            make_test(dirname)
 
 init()
