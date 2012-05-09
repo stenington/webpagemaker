904d96002de7992894128c85234e1087562bbdbc
Atul Varma <varmaa@gmail.com>
2012-05-09 17:39:08 -0400

    added a proxy to the mission slurper, so that CORS issues don't prevent fonts and such from loading.

--- webpagemaker/debugging/__init__.py
+++ webpagemaker/debugging/__init__.py
@@ -4,5 +4,8 @@ from django.conf.urls.defaults import patterns, include
 
 urlpatterns = patterns('',
     (r'^git-pull$', git.pull),
-    (r'^mission-slurp$', learning_project_slurp.slurp)
+    (r'^mission-slurp$', learning_project_slurp.slurp),
+    (r'^mission-slurp/proxy/(?P<scheme>(http|https))/' +
+     r'(?P<domain>[A-Za-z0-9\-_\.]+)/(?P<path>.*)',
+     learning_project_slurp.proxy),
 )
