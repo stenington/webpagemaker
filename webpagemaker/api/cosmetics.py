f61586385e87891ded5ddd9d48699c969421b0b8
Atul Varma <varmaa@gmail.com>
2012-05-10 11:07:50 -0400

    added more docs to cosmetics module and a link to #55.

--- webpagemaker/api/cosmetics.py
+++ webpagemaker/api/cosmetics.py
@@ -1,3 +1,21 @@
+"""
+    Bleach sanitization changes HTML in ways that are fine for delivering
+    raw HTML to a browser, but which can be confusing to the authors
+    who wrote the original HTML--particularly when the sanitized HTML
+    is semantically unchanged from the original HTML. Examples include the
+    removal of whitespace and re-ordering of element attributes.
+    
+    In the cases where authors have already written sanitized HTML--which
+    a nice front-end can help them do--we should be able to re-deliver
+    the original HTML they wrote, rather than giving them mangled yet
+    semantically-identical HTML. This module provides logic to enable
+    us to do this.
+    
+    For more information and discussion, see:
+    
+    https://github.com/mozilla/webpagemaker/issues/55
+"""
+
 import html5lib
 from html5lib import treebuilders, treewalkers, serializer
 
@@ -15,6 +33,11 @@ def _normalize(html):
     s = serializer.htmlserializer.HTMLSerializer(omit_optional_tags=False)
     output_generator = s.serialize(stream)
 
+    # TODO: We're not actually collapsing *all* whitespace; only
+    # entire chunks of whitespace that the serializer gives us. Currently,
+    # this seems "good enough" to pass our unit tests, which are
+    # based on use cases of comparing pre-sanitized HTML to sanitized HTML,
+    # but we may need to change this in the future.
     parts = []
     last_item_was_whitespace = False
     for item in output_generator:
