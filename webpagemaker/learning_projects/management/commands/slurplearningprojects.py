80af70c37ff1de807378c44b9b6e79831648294e
Brian J Brennan <brianloveswords@gmail.com>
2012-06-11 14:46:53 -0400

    Allow arguments to be passed to `slurplearningprojects`
    [Issue #379]
    
    This allows single-slurpage (or filtered-slurpage if mutliple project
    names are passed in). If no arguments are passed, it defaults to
    the previous behavior of slurping all projects from the learning
    projects directory.
    
    For example, `python manage.py slurplearningprojects stuck` would
    just slurp any directory named `stuck`.
    
    `python manage.py slurplearningprojects map meme does_not_exist`
    would warn about not being able to match all filters, then slurp map
    and meme.

--- webpagemaker/learning_projects/management/commands/slurplearningprojects.py
+++ webpagemaker/learning_projects/management/commands/slurplearningprojects.py
@@ -1,15 +1,27 @@
 import os
 import shutil
 
-from django.core.management.base import NoArgsCommand, CommandError
+from django.core.management.base import BaseCommand, CommandError
 from django.conf import settings
 
-def slurp(fromdir, stdout):
+def slurp(fromdir, stdout, project_filter):
     projects = [
       dirname for dirname in os.listdir(fromdir)
       if os.path.isdir(os.path.join(fromdir, dirname))
       and not dirname.startswith('.')
       ]
+    
+    if project_filter:
+        projects = filter(lambda n: n in project_filter, projects)
+    
+        if len(projects) is 0:
+            stdout.write('Could not find any projects that match the filters %s.\n'
+                         % str(project_filter))
+            exit(1)
+
+        if len(projects) != len(project_filter):
+            stdout.write('WARNING: did not match all filters, %s\n' % str(project_filter))
+    
     mydir = os.path.dirname(__file__)
     appdir = os.path.normpath(os.path.join(mydir, '..', '..'))
     templatedir = os.path.join(appdir, 'templates', 'learning_projects')
@@ -30,14 +42,15 @@ def slurp(fromdir, stdout):
         shutil.copytree(os.path.join(dirname, 'static'), staticdir)
     stdout.write("Done.\n")
 
-class Command(NoArgsCommand):
+class Command(BaseCommand):
+    args = '[project_name, project_name, ...]'
     help = 'Copies files from the learning projects dropbox into ' \
            'the learning_projects django app'
 
-    def handle_noargs(self, *args, **options):
+    def handle(self, *args, **options):
         if not hasattr(settings, 'LEARNING_PROJECTS_PATH'):
             raise CommandError("Please set the LEARNING_PROJECTS_PATH " \
                                "setting to point to the learning " \
                                "projects dropbox.")
-        slurp(fromdir=settings.LEARNING_PROJECTS_PATH, stdout=self.stdout)
+        slurp(fromdir=settings.LEARNING_PROJECTS_PATH, stdout=self.stdout, project_filter=args)
         self.stdout.write("You should probaby run 'git status' now.\n")
