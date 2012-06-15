f55a8cd76def068e83964cb48c43f9bf1fb2e79f
Chris McAvoy <chris.mcavoy@gmail.com>
2012-06-15 14:53:13 -0500

    adding throttle to publish

---
+++ webpagemaker/api/decorators.py
@@ -0,0 +1,18 @@
+from django.core.cache import cache
+from django.http import HttpResponseBadRequest, HttpResponseForbidden
+import hashlib
+
+def throttle_view(func, methods=None, duration=15):
+    def inner(request, *args, **kwargs):
+        throttled_methods = methods if methods else ['POST', 'GET']
+        if request.method in throttled_methods:
+            remote_addr = request.META.get('HTTP_X_FORWARDED_FOR') or \
+                          request.META.get('REMOTE_ADDR')
+            key = (hashlib.md5('%s.%s' % (remote_addr, request.path_info))
+                   .hexdigest())
+            if cache.get(key):
+                return HttpResponseForbidden('Please try again later.')
+            else:
+                cache.set(key, True, duration)
+        return func(request, *args, **kwargs)
+    return inner
