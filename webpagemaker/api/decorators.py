948c669f8b4098bd67950f71e4c0abbdf886c735
Chris McAvoy <chris.mcavoy@gmail.com>
2012-06-16 13:36:43 -0500

    removing some odd redundancy

--- webpagemaker/api/decorators.py
+++ webpagemaker/api/decorators.py
@@ -2,14 +2,13 @@ from django.core.cache import cache
 from django.http import HttpResponseBadRequest, HttpResponseForbidden
 import hashlib
 
-def throttle_view(func, methods=None, duration=15):
+def throttle_view(func, methods=['POST', 'GET'], duration=15):
     """If I was a better programmer, I would get this to work with passed arguments,
     however - it's just not in the cards tonight. hence ...
     TODO: make this thing actually take arguments
     """
     def inner(request, *args, **kwargs):
-        throttled_methods = methods if methods else ['POST', 'GET']
-        if request.method in throttled_methods:
+        if request.method in methods:
             remote_addr = request.META.get('HTTP_X_FORWARDED_FOR') or \
                           request.META.get('REMOTE_ADDR')
             key = (hashlib.md5('%s.%s' % (remote_addr, request.path_info))
