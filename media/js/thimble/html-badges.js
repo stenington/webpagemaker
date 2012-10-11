c879950b040e57731310d192586e3b6c0de0e5a1
Atul Varma <varmaa@gmail.com>
2012-10-11 16:36:04 -0400

    Added html-badges.js and test suite.
    Implementation is based on:
    
    https://github.com/toolness/friendlycode/blob/badges-spike/index.html#L211

---
+++ media/js/thimble/html-badges.js
@@ -0,0 +1,70 @@
+define(function() {
+  var behaviorQueries = {
+    HYPERLINK: function(doc) {
+      var anchors = doc.querySelectorAll("a");
+      var candidates = [];
+      
+      for (var i = 0; i < anchors.length; i++)
+        if ((anchors[i].children.length ||
+             anchors[i].textContent.trim()) &&
+            anchors[i].hasAttribute('href') &&
+            anchors[i].getAttribute('href').match(/^https?:/i))
+          // TODO: Check href to make sure entire URL is well-formed.
+          candidates.push({
+            node: anchors[i],
+            start: anchors[i].parseInfo.openTag.start,
+            end: anchors[i].parseInfo.closeTag.end
+          });
+      
+      return candidates;
+    },
+    HTML_COMMENT: function(doc) {
+      var candidates = [];
+      
+      function traverse(element) {
+        for (var i = 0; i < element.childNodes.length; i++) {
+          var node = element.childNodes[i];
+          
+          if (node.nodeType == element.ELEMENT_NODE)
+            traverse(node);
+          else if (node.nodeType == element.COMMENT_NODE &&
+                   node.nodeValue.trim())
+            candidates.push({
+              node: node,
+              start: node.parseInfo.start,
+              end: node.parseInfo.end
+            });
+        }
+      }
+      
+      traverse(doc);
+      return candidates;
+    }
+  };
+  
+  return {
+    attachProbes: function(parsingCodeMirror, badger) {
+      var originalCode = parsingCodeMirror.getValue();
+      
+      parsingCodeMirror.on("reparse", function(event) {
+        var currentCode = parsingCodeMirror.getValue();
+
+        if (event.error)
+          return;
+        
+        Object.keys(behaviorQueries).forEach(function(behavior) {
+          var candidates = behaviorQueries[behavior](event.document);
+          
+          candidates.forEach(function(info) {
+            var snippet = currentCode.slice(info.start, info.end);
+            if (originalCode.indexOf(snippet) == -1) {
+              badger.credit(behavior);
+              originalCode = currentCode;
+              //console.log("credit " + behavior + " for " + snippet);
+            }
+          });
+        });
+      });
+    }
+  };
+});
---
+++ media/test/test-html-badges.js
@@ -0,0 +1,73 @@
+"use strict";
+
+defineTests([
+  "thimble/html-badges",
+  "backbone-events",
+  "slowparse/slowparse"
+], function(HTMLBadges, BackboneEvents, Slowparse) {
+  module("html-badges");
+  
+  function HTMLBadgeTest(name, options) {
+    test(name, function() {
+      var expectedCredits = options.credits || [];
+      var originalHTML = options.beginHTML;
+      var modifiedHTML = options.endHTML;
+      var currentHTML = originalHTML;
+      var credits = [];
+      var badger = {
+        credit: function(behavior) {
+          credits.push(behavior);
+        }
+      };
+      var parsingCodeMirror = BackboneEvents.mixin({
+        getValue: function() {
+          return currentHTML;
+        }
+      });
+    
+      HTMLBadges.attachProbes(parsingCodeMirror, badger);
+
+      ok(originalHTML != modifiedHTML,
+         "begin snippet (" + parsingCodeMirror.getValue() +
+         ") is different from end snippet");
+
+      currentHTML = modifiedHTML;
+      var result = Slowparse.HTML(document, parsingCodeMirror.getValue());
+
+      ok(!result.error,
+         "end snippet has no errors: " + parsingCodeMirror.getValue());
+      parsingCodeMirror.trigger('reparse', result);
+      deepEqual(credits, expectedCredits,
+                expectedCredits.length ? 
+                "behavior(s) " + expectedCredits.join(', ') + " credited" :
+                "no behaviors are credited");
+    });
+  }
+
+  HTMLBadgeTest("valid hyperlinks are credited", {
+    beginHTML: '<p>hello</p>',
+    endHTML: '<p>hello <a href="http://foo.org">there</a></p>',
+    credits: ['HYPERLINK']
+  });
+  
+  HTMLBadgeTest("empty hyperlinks are not credited", {
+    beginHTML: '<p>hello</p>',
+    endHTML: '<p>hello <a href="http://foo.org"></a></p>'
+  });
+
+  HTMLBadgeTest("hyperlinks w/ invalid hrefs are not credited", {
+    beginHTML: '<p>hello</p>',
+    endHTML: '<p>hello <a href="blarg">there</a></p>'
+  });
+  
+  HTMLBadgeTest("non-empty HTML comments are credited", {
+    beginHTML: '<p>hello</p>',
+    endHTML: '<p>hello <!-- comment --></p>',
+    credits: ['HTML_COMMENT']
+  });
+  
+  HTMLBadgeTest("empty HTML comments are not credited", {
+    beginHTML: '<p>hello</p>',
+    endHTML: '<p>hello <!--  --></p>'
+  });
+});
