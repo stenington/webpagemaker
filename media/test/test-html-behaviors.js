13baf465a8504cc77214077dd21d18c84c5bb442
Atul Varma <varmaa@gmail.com>
2012-11-01 09:54:58 -0400

    renamed html-badges.js to html-behaviors.js.

--- media/test/test-html-behaviors.js
+++ media/test/test-html-behaviors.js
@@ -1,13 +1,13 @@
 "use strict";
 
 defineTests([
-  "thimble/html-badges",
+  "thimble/html-behaviors",
   "backbone-events",
   "slowparse/slowparse"
-], function(HTMLBadges, BackboneEvents, Slowparse) {
-  module("thimble/html-badges");
+], function(Behaviors, BackboneEvents, Slowparse) {
+  module("thimble/html-behaviors");
   
-  function HTMLBadgeTest(name, options) {
+  function HTMLBehaviorTest(name, options) {
     test(name, function() {
       var expectedCredits = options.credits || [];
       var originalHTML = options.beginHTML;
@@ -20,7 +20,7 @@ defineTests([
         }
       });
     
-      HTMLBadges.attachProbes(parsingCodeMirror, function credit(behavior) {
+      Behaviors.attachProbes(parsingCodeMirror, function credit(behavior) {
         credits.push(behavior);
       });
 
@@ -41,141 +41,141 @@ defineTests([
     });
   }
 
-  HTMLBadgeTest("valid hyperlinks are credited", {
+  HTMLBehaviorTest("valid hyperlinks are credited", {
     beginHTML: '<p>hello</p>',
     endHTML: '<p>hello <a href="http://foo.org">there</a></p>',
     credits: ['HYPERLINK']
   });
   
-  HTMLBadgeTest("empty hyperlinks are not credited", {
+  HTMLBehaviorTest("empty hyperlinks are not credited", {
     beginHTML: '<p>hello</p>',
     endHTML: '<p>hello <a href="http://foo.org"></a></p>'
   });
 
-  HTMLBadgeTest("hyperlinks w/ invalid hrefs are not credited", {
+  HTMLBehaviorTest("hyperlinks w/ invalid hrefs are not credited", {
     beginHTML: '<p>hello</p>',
     endHTML: '<p>hello <a href="blarg">there</a></p>'
   });
   
-  HTMLBadgeTest("non-empty HTML comments are credited", {
+  HTMLBehaviorTest("non-empty HTML comments are credited", {
     beginHTML: '<p>hello</p>',
     endHTML: '<p>hello <!-- comment --></p>',
     credits: ['HTML_COMMENT']
   });
   
-  HTMLBadgeTest("empty HTML comments are not credited", {
+  HTMLBehaviorTest("empty HTML comments are not credited", {
     beginHTML: '<p>hello</p>',
     endHTML: '<p>hello <!--  --></p>'
   });
   
-  HTMLBadgeTest("changed CSS in style elements is credited", {
+  HTMLBehaviorTest("changed CSS in style elements is credited", {
     beginHTML: '<style>#foo { color: pink; }</style>',
     endHTML: '<style>#foo { color: red; }</style>',
     credits: ['CSS_CHANGED']
   });
 
-  HTMLBadgeTest("changed CSS in style attributes is credited", {
+  HTMLBehaviorTest("changed CSS in style attributes is credited", {
     beginHTML: '<span>hi</span>',
     endHTML: '<span style="color: blue">hi</span>',
     credits: ['CSS_CHANGED']
   });
 
-  HTMLBadgeTest("empty style attributes are not credited", {
+  HTMLBehaviorTest("empty style attributes are not credited", {
     beginHTML: '<span>hi</span>',
     endHTML: '<span style="">hi</span>'
   });
   
-  HTMLBadgeTest("empty style elements are not credited", {
+  HTMLBehaviorTest("empty style elements are not credited", {
     beginHTML: '<p>hi</p>',
     endHTML: '<style></style><p></p>'
   });
   
-  HTMLBadgeTest("valid iframes are credited", {
+  HTMLBehaviorTest("valid iframes are credited", {
     beginHTML: '<p>hi</p>',
     endHTML: '<p>hi <iframe src="http://foo.org/"></iframe></p>',
     credits: ['IFRAME']
   });
   
-  HTMLBadgeTest("iframes w/ invalid src attrs are not credited", {
+  HTMLBehaviorTest("iframes w/ invalid src attrs are not credited", {
     beginHTML: '<p>hi</p>',
     endHTML: '<p>hi <iframe src="blarg"></iframe></p>',
   });
 
-  HTMLBadgeTest("iframes w/o any src attr are not credited", {
+  HTMLBehaviorTest("iframes w/o any src attr are not credited", {
     beginHTML: '<p>hi</p>',
     endHTML: '<p>hi <iframe></iframe></p>',
   });
 
-  HTMLBadgeTest("changing body of an iframe doesn't credit", {
+  HTMLBehaviorTest("changing body of an iframe doesn't credit", {
     beginHTML: '<p>hi <iframe src="http://foo.org/"></iframe></p>',
     endHTML: '<p>hi <iframe src="http://foo.org/">lol</iframe></p>'
   });
   
-  HTMLBadgeTest("valid images are credited", {
+  HTMLBehaviorTest("valid images are credited", {
     beginHTML: '<p>hi</p>',
     endHTML: '<p>hi <img src="http://foo.org/"></p>',
     credits: ['IMAGE']
   });
   
-  HTMLBadgeTest("images w/o src attrs are not credited", {
+  HTMLBehaviorTest("images w/o src attrs are not credited", {
     beginHTML: '<p>hi</p>',
     endHTML: '<p>hi <img></p>',
   });
 
-  HTMLBadgeTest("valid ordered lists are credited", {
+  HTMLBehaviorTest("valid ordered lists are credited", {
     beginHTML: '<p>hi <ol></ol></p>',
     endHTML: '<p>hi <ol><li>sup</li></ol></p>',
     credits: ['LIST']
   });
 
-  HTMLBadgeTest("valid unordered lists are credited", {
+  HTMLBehaviorTest("valid unordered lists are credited", {
     beginHTML: '<p>hi <ul></ul></p>',
     endHTML: '<p>hi <ul><li>sup</li></ul></p>',
     credits: ['LIST']
   });
 
-  HTMLBadgeTest("empty list elements are not credited", {
+  HTMLBehaviorTest("empty list elements are not credited", {
     beginHTML: '<p>hi <ol></ol></p>',
     endHTML: '<p>hi <ol><li></li></ol></p>'
   });
 
-  HTMLBadgeTest("orphan list elements are not credited", {
+  HTMLBehaviorTest("orphan list elements are not credited", {
     beginHTML: '<p>hi</p>',
     endHTML: '<p>hi <li>sup</li></p>'
   });
   
   for (var i = 1; i <= 6; i++) {
-    HTMLBadgeTest("valid h" + i + " tags are credited", {
+    HTMLBehaviorTest("valid h" + i + " tags are credited", {
       beginHTML: '<p>hi</p>',
       endHTML: '<h' + i + '>yo</h' + i + '><p>hi</p>',
       credits: ['TEXT']
     });
   }
 
-  HTMLBadgeTest("changing paragraph text is credited", {
+  HTMLBehaviorTest("changing paragraph text is credited", {
     beginHTML: '<p>hi</p>',
     endHTML: '<p>hi there</p>',
     credits: ['TEXT']
   });
   
-  HTMLBadgeTest("adding a div is credited", {
+  HTMLBehaviorTest("adding a div is credited", {
     beginHTML: '<p>hi</p>',
     endHTML: '<div><p>hi</p></div>',
     credits: ['DIV']
   });
   
-  HTMLBadgeTest("adding an audio tag w/ valid src attr is credited", {
+  HTMLBehaviorTest("adding an audio tag w/ valid src attr is credited", {
     beginHTML: '<p>hi</p>',
     endHTML: '<p>hi <audio src="http://foo/"></audio></p>',
     credits: ['AUDIO']
   });
 
-  HTMLBadgeTest("adding an audio tag w/ invalid src attr is not credited", {
+  HTMLBehaviorTest("adding an audio tag w/ invalid src attr isn't credited", {
     beginHTML: '<p>hi</p>',
     endHTML: '<p>hi <audio src="gweg"></audio></p>'
   });
   
-  HTMLBadgeTest("adding an audio tag w/ valid source tag is credited", {
+  HTMLBehaviorTest("adding an audio tag w/ valid source tag is credited", {
     beginHTML: '<p>hi</p>',
     endHTML: '<p>hi <audio><source src="http://foo.org/"></audio></p>',
     credits: ['AUDIO']
