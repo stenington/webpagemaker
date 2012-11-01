e69f8bbab679dbe06ee6a5a0219b317d2d64f33d
Atul Varma <varmaa@gmail.com>
2012-11-01 13:29:21 -0400

    added behavior testbed to behaviors.html.

---
+++ media/test/require-test-config.js
@@ -0,0 +1,4 @@
+// These paths are a bit odd because they're relative to
+// ../friendlycode/js, not the directory this file is in.
+require.paths['thimble'] = '../../js/thimble';
+require.paths['thimble-test'] = '../../test';
